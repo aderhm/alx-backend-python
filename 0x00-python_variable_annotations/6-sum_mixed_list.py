8102d92d6c9a6946e8f356a2e1e9e8a2824e07b4
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-07-04 18:39:32 +0100

    Implement typed-annotated function: sum_mixed_list()

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[int | float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Returns the sum of the floats in the list
     """
     return sum(mxd_lst)
